6ca2736c46b4ed9febb116cfbeda9f773fef83f0
jaleisme <jaleisme.id@gmail.com>
2023-05-10 13:05:36 +0700

    Init project

---
+++ zakat-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <span>Ini landing aplikasi zakat</span>
+    </div>
+</div>
+@endsection
