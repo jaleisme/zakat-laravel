d1b6c0cd2242680cc3d9a77bd8041e197442bf2d
jaleisme <jaleisme.id@gmail.com>
2023-05-14 20:13:53 +0700

    Initial Commit

---
+++ database/db-analysis.md
@@ -0,0 +1,37 @@
+mustahik_category
+id
+category_name
+description
+percentage
+
+mustahik
+Id
+mustahik_category_id
+fullname
+address
+
+payment_type
+id
+name
+
+muzakki
+id
+fullname
+address
+
+payment
+id
+muzakki_id
+payment_type_id
+amount
+number_of_people
+shodaqoh 
+
+distribution: [(percentage*total zakat per type)/mustahik according to type]
+id
+mustahik_id
+amount_money
+amount_rice
+notes
+distributed_at
+status 0:Not Distributed, 1:On Progress, 2:Distributed, 3:Partially Distributed, 4: Pending
---
+++ database/zakat-db.sql
@@ -0,0 +1,331 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2023 at 03:08 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `zakat-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `distribution`
+--
+
+CREATE TABLE `distribution` (
+  `id` int(11) NOT NULL,
+  `mustahik_id` int(10) UNSIGNED NOT NULL,
+  `amount_money` int(11) NOT NULL,
+  `amount_rice` int(11) NOT NULL,
+  `notes` tinytext NOT NULL,
+  `distributed_at` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mustahik`
+--
+
+CREATE TABLE `mustahik` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `mustahik_category_id` int(10) UNSIGNED NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `address` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mustahik_category`
+--
+
+CREATE TABLE `mustahik_category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `category_name` varchar(50) NOT NULL,
+  `description` tinytext NOT NULL,
+  `percentage` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `muzakki`
+--
+
+CREATE TABLE `muzakki` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `address` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `muzakki_id` int(10) UNSIGNED NOT NULL,
+  `payment_type_id` int(10) UNSIGNED NOT NULL,
+  `amount` int(11) NOT NULL,
+  `number_of_person` int(11) NOT NULL,
+  `infaq` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_type`
+--
+
+CREATE TABLE `payment_type` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `payment_type_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin@admin.com', NULL, '$2y$10$K55wbch3a3Ayn863yaIEXO73dgsSXmehwOC3eVi2M2FTSxT6nld1.', NULL, '2023-05-09 21:13:05', '2023-05-09 21:13:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `distribution`
+--
+ALTER TABLE `distribution`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `mustahik_id` (`mustahik_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mustahik`
+--
+ALTER TABLE `mustahik`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `mustahik_category_id` (`mustahik_category_id`);
+
+--
+-- Indexes for table `mustahik_category`
+--
+ALTER TABLE `mustahik_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `muzakki`
+--
+ALTER TABLE `muzakki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `muzakki_id` (`muzakki_id`),
+  ADD KEY `payment_type_id` (`payment_type_id`);
+
+--
+-- Indexes for table `payment_type`
+--
+ALTER TABLE `payment_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `distribution`
+--
+ALTER TABLE `distribution`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `mustahik`
+--
+ALTER TABLE `mustahik`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mustahik_category`
+--
+ALTER TABLE `mustahik_category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `muzakki`
+--
+ALTER TABLE `muzakki`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_type`
+--
+ALTER TABLE `payment_type`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `distribution`
+--
+ALTER TABLE `distribution`
+  ADD CONSTRAINT `FK_distribution_mustahik` FOREIGN KEY (`mustahik_id`) REFERENCES `mustahik` (`id`);
+
+--
+-- Constraints for table `mustahik`
+--
+ALTER TABLE `mustahik`
+  ADD CONSTRAINT `FK_mustahik_mustahik_categories` FOREIGN KEY (`mustahik_category_id`) REFERENCES `mustahik_category` (`id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `FK_payment_muzakki` FOREIGN KEY (`muzakki_id`) REFERENCES `muzakki` (`id`),
+  ADD CONSTRAINT `FK_payment_payment_type` FOREIGN KEY (`payment_type_id`) REFERENCES `payment_type` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
