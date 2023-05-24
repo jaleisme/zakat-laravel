06909bf56c7e78cefae8418b5a9e7d07cb1bfbbe
jaleisme <jaleisme.id@gmail.com>
2023-05-24 09:20:55 +0700

    Landing Page Redesign

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,18 +16,26 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        body{
+            font-family: 'Montserrat', sans-serif !important;
+            font-size: 12pt !important;
+        }
+    </style>
     @yield('custom-css')
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand font-weight-bold" href="{{ url('/') }}">
-                    My<span class="text-success">{{ config('app.name', 'Laravel') }}</span>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <span class="font-weight-bold">
+                        My<span class="text-success">{{ config('app.name', 'Laravel') }}</span>
+                    </span>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,47 @@
 @extends('layouts.app')
 
+@section('custom-css')
+<style>
+    .jumbotron{
+        background:
+        /* top, transparent black, faked with gradient */
+        linear-gradient(
+          rgba(0, 0, 0, 0.5),
+          rgba(0, 0, 0, 0.5)
+        ),
+        /* bottom, image */
+        url('/bg-landing.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        color: #ffffff;
+        height: 70vh;
+        margin-top: -48px;
+    }
+</style>
+@endsection
+
 @section('content')
+<div class="jumbotron d-flex align-items-center">
+    <div class="container">
+        <h1 class="display-5 font-weight-bold">Manage Zakat Collection without any hassle!</h1>
+        <p>Having a hard time managing zakat collection? Let MyZakat handle that.</p>
+        <a class="btn btn-primary btn-lg mt-3" style="font-size: 14px;" href="#about" role="button">Learn More</a>
+    </div>
+</div>
 <div class="container">
-    <div class="row justify-content-center">
-        <span class="font-weight-bold text-muted">Under Construction.</span>
+    <div class="row my-3">
+        <div class="col-12">
+            <h3 class="font-weight-bold">Zakat <span>Fitrah</span></h3>
+            <p class="text-justify">"Those who believe, do righteous deeds, establish prayer, and give zakah, will have their reward with their Lord; and they will have no fear, nor will they grieve." <br>Al-Baqarah : 277.</p>
+        </div>
+    </div>
+    <div class="row my-3" id="about">
+        <div class="col-12 text-justify">
+            <h3 class="font-weight-bold">What is Zakat?</h3>
+            <p>As one of the Five Pillars of Islam, zakat is a religious duty for all Muslims who meet the necessary criteria of wealth to help the needy. It is a mandatory charitable contribution, often considered to be a tax. The payment and disputes on zakat have played a major role in the history of Islam, notably during the Ridda wars.</p>
+            <p>Zakat on wealth is based on the value of all of one's possessions. It is customarily 2.5% (or 1/4) of a Muslim's total savings and wealth above a minimum amount known as nisab each lunar year, but Islamic scholars differ on how much nisab is and other aspects of zakat. According to Islamic doctrine, the collected amount should be paid to the poor and the needy, Zakat collectors, orphans, widows, those to be freed from slavery, old-aged peoples who can't work to feed themselves, those in debt, in the cause of Allah and to benefit the stranded traveller.</p>
+        </div>
     </div>
 </div>
 @endsection
